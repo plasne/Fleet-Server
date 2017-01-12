7e664054130ec2f49358bd8250118b6db157e400
plasne <plasne@Peters-iMac.local>
2017-01-12 17:45:17 -0500

    added expiry

--- match/index.js
+++ match/index.js
@@ -2,7 +2,7 @@
 const redis = require("redis");
 const uuid = require("node-uuid");
 
-const verbose = false;
+const verbose = true;
 
 module.exports = function(context) {
 
@@ -10,14 +10,36 @@ module.exports = function(context) {
     var redis_key = process.env["RedisKey"];
     var client = redis.createClient(6380, "pelasne-fleet.redis.cache.windows.net", {
         auth_pass: redis_key,
-        tls: { servername: "pelasne-fleet.redis.cache.windows.net" }
+        tls: { servername: "pelasne-fleet.redis.cache.windows.net" },
+        retry_strategy: function(options) {
+            if (options.error && options.error.code === "ECONNREFUSED") {
+                return new Error("The server refused the connection");
+            }
+            if (options.total_retry_time > 1000 * 60 * 1) { // 1 min
+                return new Error("Retry time exhausted");
+            }
+            if (options.times_connected > 10) {
+                return undefined;
+            }
+            return Math.min(options.attempt * 100, 3000);
+        }
     });
 
     // get the request information
     var playerId = context.req.headers["player"];
     var opponentId = context.req.headers["opponent"];
     var group = context.req.headers["group"];
-    if (playerId != null && group != null) {
+    if (playerId != null && opponentId != null && playerId == opponentId) {
+
+        // ERROR: invalid opponent
+        context.log("a player tried to face-off with himself.");
+        context.res = {
+            status: 500,
+            body: "invalid_opponent"
+        }
+        context.done();
+
+    } else if (playerId != null) {
 
         // see if the player has been paired
         client.get("player:" + playerId, function(err, gameInfo) {
@@ -37,7 +59,50 @@ module.exports = function(context) {
                         context.done();
                     });
 
-                } else {
+                } else if (opponentId != null) {
+
+                    // store a game entry for the specified player
+                    if (verbose) context.log("a match is created since an opponent was specified.");
+                    var gameId = uuid.v4();
+                    client.set("player:" + opponentId, JSON.stringify({
+                        status: "matched",
+                        gameId: gameId,
+                        opponentId: playerId
+                    }), function(err) {
+                        if (!err) {
+
+                            // set the expiry for 10 min
+                            client.expire("player:" + opponentId, 60 * 10, function(err) {
+                                if (err) context.log("player:" + opponentId + " expiry could not be set.");
+
+                                // return that this player is waiting on the other
+                                context.res = {
+                                    status: 200,
+                                    body: {
+                                        status: "matched",
+                                        gameId: gameId,
+                                        opponentId: opponentId
+                                    }
+                                }
+                                context.done();
+
+                            });
+
+                        } else {
+
+                            // ERROR: cannot add a game entry for the player
+                            context.log("cannot add a game entry for a player.");
+                            context.res = {
+                                status: 500,
+                                body: "cannot_store_game_entry"
+                            }
+                            context.done();
+
+                        }
+                        
+                    });
+
+                } else if (group != null) {
 
                     // the player has not been matched yet, so see if there is someone waiting
                     client.get("lobby:" + group, function(err, playerInLobbyId) {
@@ -60,35 +125,47 @@ module.exports = function(context) {
                                         }), function(err) {
                                             if (!err) {
 
-                                                // return the game info to the requesting player
-                                                context.res = {
-                                                    status: 200,
-                                                    body: {
-                                                        status: "matched",
-                                                        gameId: gameId,
-                                                        opponentId: playerInLobbyId
+                                                // set the expiry for 10 min
+                                                client.expire("player:" + playerInLobbyId, 60 * 10, function(err) {
+                                                    if (err) context.log("player:" + playerInLobbyId + " expiry could not be set.");
+
+                                                    // return the game info to the requesting player
+                                                    context.res = {
+                                                        status: 200,
+                                                        body: {
+                                                            status: "matched",
+                                                            gameId: gameId,
+                                                            opponentId: playerInLobbyId
+                                                        }
                                                     }
-                                                }
-                                                context.done();
+                                                    context.done();
+
+                                                });
 
                                             } else {
+
+                                                // ERROR: cannot add a game entry for the player
                                                 context.log("cannot add a game entry for a player.");
                                                 context.res = {
                                                     status: 500,
                                                     body: "cannot_store_game_entry"
                                                 }
                                                 context.done();
+
                                             }
                                             
                                         });
 
                                     } else {
+
+                                        // ERROR: cannot remove the player from the lobby
                                         context.log("cannot remove player from lobby");
                                         context.res = {
                                             status: 500,
                                             body: "cannot_remove_player"
                                         }
                                         context.done();
+
                                     }
                                 });
 
@@ -109,43 +186,69 @@ module.exports = function(context) {
                             // there is no one in the lobby, so put this player there
                             client.set("lobby:" + group, playerId, function(err) {
                                 if (!err) {
-                                    if (verbose) context.log("the player was queued in the lobby.");
-                                    context.res = {
-                                        status: 200,
-                                        body: { status: "waiting" }
-                                    }
-                                    context.done();
+
+                                    // set the expiry to 10 min
+                                    client.expire("lobby:" + group, 60 * 10, function(err) {
+                                        if (err) context.log("lobby:" + group + " expiry could not be set.");
+
+                                        // queue the player in the lobby
+                                        if (verbose) context.log("the player was queued in the lobby.");
+                                        context.res = {
+                                            status: 200,
+                                            body: { status: "enqueued" }
+                                        }
+                                        context.done();
+
+                                    });
+
                                 } else {
+
+                                    // ERROR: player cannot be put in the lobby
                                     context.log("cannot put a player in the lobby");
                                     context.res = {
                                         status: 500,
                                         body: "cannot_put_in_lobby"
                                     }
                                     context.done();
+                                    
                                 }
                             });
 
                         }
                     });
 
+                } else {
+
+                    // group is null, so just sit and wait on someone to match with him
+                    context.res = {
+                        status: 200,
+                        body: { status: "deferred" }
+                    }
+                    context.done();
+
                 }
             } else {
+
+                // ERROR: cannot query for the player to see if he has been matched already
                 context.log("could not query for the player.");
                 context.res = {
                     status: 500,
-                    body: "cannot_query_players"
+                    body: "cannot_query_player"
                 }
                 context.done();
             }
         });
 
     } else {
+
+        // ERROR: invalid parameters
         context.log("playerId or group was null so the request was ignored.");
         context.res = {
             status: 500,
             body: "invalid_parameters"
         }
         context.done();
+
     }
 
 }
